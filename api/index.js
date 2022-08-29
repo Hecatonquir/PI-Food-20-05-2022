c56b9c4fb53c8f98519b52c4acd7a92a99cf23bb
Hecatonquir <maxicuentas92@hotmail.com>
2022-08-29 20:47:11 -0300

    changed api index

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require('./src/db.js');
 const { upDietTypes, getApiRecipes } = require('./src/functions.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
 	server.listen(process.env.PORT, async () => {
 		console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
 		await upDietTypes();
